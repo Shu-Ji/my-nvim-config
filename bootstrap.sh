c1ab4dc4d6edc3ac2bdc8b9e1fd3316f4eb2eff9
shuji <bh2@qq.com>
2020-05-10 22:31:41 +0800

    update

--- bootstrap.sh
+++ bootstrap.sh
@@ -9,6 +9,7 @@ mkdir -p $nvim
 
 ln -s $me/init.vim $nvim/init.vim
 
+
 brew install martindelille/tap/qmlfmt ripgrep
 sudo apt-get install ripgrep
 
@@ -44,3 +45,11 @@ yarn global add alex
 
 # js 自动 import
 yarn global add import-js
+
+
+# 安装 vim-plug
+sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+
+# 安装所有插件
+vim +PlugInstall +qall
